458b400dd3daad60ec7bd6efd0b93d139aeb7f39
laryol <laryol2@gmx.de>
2021-12-18 11:39:03 +0100

    FE/BE2

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import express from 'express';
+import axios from "axios";
 const app = express()
 const cors = require('cors')
-app.use(cors())
 const port = process.env.PORT || 3000
+app.use(cors())
 
 
 app.get('/api', (req, res) => {
     res.status(200).json({api: 'version 1'})
 })
+axios.get(`'https://webtechbackend.herokuapp.com/api'`,{headers: {'Access-Control-Allow-Origin': '*'}})
+    .then(response => console.log(response.data))
+
+app.use((req, res) => res.status(404).send({code: '404', message: 'no found'}))
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:`,port)
+    console.log(`Example app listening at http://localhost:`, port)
 })
-
-
-
 
 
 /* import express from 'express';
