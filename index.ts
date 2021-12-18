09753ebf85014860bac84d1be64f3570fde2d1a1
laryol <laryol2@gmx.de>
2021-12-18 11:57:10 +0100

    Acess-Control

--- index.ts
+++ index.ts
@@ -5,6 +5,14 @@ const cors = require('cors')
 const port = process.env.PORT || 3000
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 
 app.get('/api', (req, res) => {
     res.status(200).json({api: 'version 1'})
