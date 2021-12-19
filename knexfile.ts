571cd50c588e404f5eac478ad9d521c55123ae02
loanya <s0556260@htw-berlin.de>
2021-12-19 11:05:49 +0100

    add user

--- knexfile.ts
+++ knexfile.ts
@@ -80,25 +80,4 @@ const config: Knex.Config = {
 };
 
 export default config;
-
-
- */
-/*
-import { Knex } from "knex"
-const config: Knex.Config = {
-    client: "postgresql",
-    connection: {
-        host : 'redis-15713.c135.eu-central-1-1.ec2.cloud.redislabs.com',
-        port: 15713
-    },
-
-    pool: {
-        min: 2,
-        max: 10
-    },
-    migrations: {
-        tableName: "knex_migrations"
-    }
-};
-export default config;
 */
