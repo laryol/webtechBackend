04d3757a3fa940577138ae5e0f5886c60da8c97f
loanya <s0556260@htw-berlin.de>
2021-12-19 15:59:25 +0100

    WIP cors

--- index.ts
+++ index.ts
@@ -1,53 +1,32 @@
-import express, { Request } from "express";
-import cors from "cors";
+import express from "express";
+
 import AuthService from "./services/AuthService";
+import cookieParser from "cookie-parser";
+
+import cors from "cors";
 
 
-//const cors = require('cors')
-
-
-const port = process.env.PORT || 3000
 const app = express()
+const port = process.env.PORT || 3000
+
 const authService = new AuthService();
-//initialisieren AuthService
 
-//grau heißt wird nicht benutzt
-const checkLogin = async (
-    req: Request,
-    res: express.Response,
-    next: express.NextFunction
-) => {
-    const session = req.cookies.session;
-    if (!session) {
-        res.status(401);
-        return res.json({ message: "You need to be logged in to see this page." });
-    }
-    const email = await authService.getUserEmailForSession(session);
-    if (!email) {
-        res.status(401);
-        return res.json({ message: "You need to be logged in to see this page." });
-    }
-    req.userEmail = email;
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-    next();
-};
-
-app.use(cors({
-    origin: true,
-    credentials: true,
-}))
 
 app.get('/', (req, res) => {
     res.send({headers: req.headers})
 })
 
-//aus Herrn Hühnes Server Beispiel
-app.post("/login", async (req, res) => { //request kommt an
-    const payload = req.body; //lesen der Daten des Requests
-    const sessionId = await authService.login(payload.email, payload.password); //bekommen sessionId mit den Daten
+app.post("/login", async (req, res) => {
+    const payload = req.body;
+
+    const sessionId = await authService.login(payload.email, payload.password);
     if (!sessionId) {
         res.status(401);
-        return res.json({ message: "Bad email or password" });
+        return res.json({message: "Bad email or password"});
     }
     res.cookie("session", sessionId, {
         maxAge: 60 * 60 * 1000,
@@ -55,7 +34,7 @@ app.post("/login", async (req, res) => { //request kommt an
         sameSite: "none",
         secure: process.env.NODE_ENV === "production",
     });
-    res.json({ status: "ok" });
+    res.json({status: "ok"});
 });
 
 app.listen(port, () => {
