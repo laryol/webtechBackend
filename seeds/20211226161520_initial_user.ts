03851fecdbabb7ae6a8bf0b922dd63262a7fa7ee
loanya <s0556260@htw-berlin.de>
2021-12-30 14:25:10 +0100

    Changed seed file

--- seeds/20211226161520_initial_user.ts
+++ seeds/20211226161520_initial_user.ts
@@ -1,10 +1,4 @@
 import { Knex } from "knex";
-import axios from "axios";
-
-
-
-axios.get(`'https://webtechbackend.herokuapp.com/api'`,{headers: {'Access-Control-Allow-Origin': '*'}})
-    .then(response => console.log(response.data))
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
